a7ce0eadc2795e0aab6da7bab90a519164c79d25
Joe Love <joe.love@musicglue.com>
2016-01-28 13:36:18 +0000

    fix js, add desktop styles

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 $(function() {
-	var headerClass = 'header__title';
-	var sectionClass = 'section';
+  var headerClass = 'header__title';
+  var sectionClass = 'section';
 
-	var sectionHiddenClass = 'section--hidden';
-	var sectionAboutClass = 'section--about';
+  var sectionHiddenClass = 'section--hidden';
+  var sectionAboutClass = 'section--about';
 
-	var navigationClass = 'navigation__link';
-	var navigationSelectedClass = 'navigation__link--selected';
-	var navigationAboutClass = 'navigation__link--about';
+  var navigationClass = 'navigation__link';
+  var navigationSelectedClass = 'navigation__link--selected';
+  var navigationAboutClass = 'navigation__link--about';
 
-	function _(className) { return $('.' + className) }
+  function _(className) { return $('.' + className) }
 
-	_(headerClass).hide();
-	_(sectionClass).addClass(sectionHiddenClass);
+  _(headerClass).hide();
+  _(sectionClass).addClass(sectionHiddenClass);
 
-	_(sectionAboutClass).removeClass(sectionHiddenClass);
-	_(navigationAboutClass).addClass(navigationSelectedClass);
+  _(sectionAboutClass).removeClass(sectionHiddenClass);
+  _(navigationAboutClass).addClass(navigationSelectedClass);
 
-	_(navigationClass).click(function(event) {
-		var _target = $(event.target);
-		var _selector = $(_target.attr('href'));
+  _(navigationClass).click(function(event) {
+    var _target = $(event.target);
+    var _selector = $(_target.attr('href'));
 
-		_(navigationSelectedClass).removeClass(navigationSelectedClass);
-		_target.addClass(navigationSelectedClass);
+    _(navigationSelectedClass).removeClass(navigationSelectedClass);
+    _target.addClass(navigationSelectedClass);
 
-		_(sectionClass).addClass(sectionHiddenClass);
-		_selector.removeClass(sectionHiddenClass);
+    _(sectionClass).addClass(sectionHiddenClass);
+    _selector.removeClass(sectionHiddenClass);
 
-		event.preventDefault();
-	});
+    event.preventDefault();
+  });
 });
